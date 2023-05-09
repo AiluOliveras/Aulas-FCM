bafbfda386380b39d6fdc79aa7a342a5a997e9ed
DdEze <ezequielensenada@yahoo.com.ar>
2023-05-09 16:38:42 -0300

    3.10 string

--- aulasfcm/views/calendarView.py
+++ aulasfcm/views/calendarView.py
@@ -66,12 +66,14 @@ def get_date(req_day):
 def prev_month(d):
     first = d.replace(day=1)
     prev_month = first - timedelta(days=1)
-    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    #month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    month = f'month={str(prev_month.year)}-{str(prev_month.month)}'
     return month
 
 def next_month(d):
     days_in_month = calendar.monthrange(d.year, d.month)[1]
     last = d.replace(day=days_in_month)
     next_month = last + timedelta(days=1)
-    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    #month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    month = f'month={str(next_month.year)}-{str(next_month.month)}'
     return month
