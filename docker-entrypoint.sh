558131e8030f0f3907ff5c3d5392b97f306827dc
AiluOliveras <ailen_oliveras7@hotmail.com>
2023-05-09 22:19:08 -0300

    Agrego comando a dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,10 @@
 # echo "Collect static files"
 # python manage.py collectstatic --noinput
 
-# Apply database migrations
-echo "Apply database migrations"
+echo "Creando migraciones"
+python manage.py makemigrations
+echo "Aplicando migraciones"
 python manage.py migrate
 
-# Start server
-echo "Starting server"
+echo "Iniciando server"
 python manage.py runserver 0.0.0.0:8000
